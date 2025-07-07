0d99b6e5a35fefcfcd9d674b7c91c708596387fd
milandiaz <milandiaz2000@hotmail.com>
2025-07-07 12:15:14 -0500

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+import { useState } from "react";
+import { shows } from "./shows/data";
+import ShowDetails from "./shows/ShowDetails";
+import ShowSelection from "./shows/ShowSelection";
+
 export default function App() {
   return (
     <>
       <header>
         <p>React TV</p>
+        <ShowSelection />
       </header>
-      <main></main>
+      <main>
+        <ShowDetails />
+      </main>
     </>
   );
 }
