61162e70c512c30d96229ec67defa1687ca38d3d
milandiaz <milandiaz2000@hotmail.com>
2025-07-07 12:25:30 -0500

    Added some code

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,20 @@
 import "./shows.css";
+import { useState } from "react";
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
 
 /** Allows users to browse through the episodes of the given show */
 export default function ShowDetails(show) {
-  return <div className="show-details"></div>;
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+
+  if (!show) {
+    return <p>Please select a show.</p>;
+  }
+  return (
+    <div className="show-details">
+      <h1>{show.name}</h1>
+      <EpisodeList />
+      <EpisodeDetails />
+    </div>
+  );
 }
